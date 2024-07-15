f63bdd2b3043e8b1629b061dd3375bbe56bea9c3
jesseponkm <thuatdthe173070@fpt.edu.vn>
2024-07-15 14:13:29 +0700

    add DAO

---
+++ Data Access Layer/BrandDAO.cs
@@ -0,0 +1,28 @@
+﻿using BusinessObjects.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Access_Layer
+{
+    public class BrandDAO
+    {
+        private static PhoneWarehouseDbContext context = new PhoneWarehouseDbContext();
+        private BrandDAO() { }
+        public static List<Brand> GetBrands()
+        {
+            var brands = new List<Brand>();
+            try
+            {
+                brands = context.Brands.ToList();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return brands;
+        }
+    }
+}
