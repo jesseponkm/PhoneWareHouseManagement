bb42e69f6bd04e19e0a4686d31012ab8cd28f73f
kietqthe172485 <kietqthe172485@fpt.edu.vn>
2024-07-15 13:42:47 +0700

    update context

--- BusinessObjects/Models/PhoneWarehouseDbContext.cs
+++ BusinessObjects/Models/PhoneWarehouseDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace BusinessObjects.Models;
 
@@ -32,9 +33,13 @@ public partial class PhoneWarehouseDbContext : DbContext
     public virtual DbSet<Supplier> Suppliers { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=localhost;database=PhoneWarehouseDB;Trusted_Connection=SSPI;Encrypt=false;TrustServerCertificate=true");
-
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            var connectionString = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetConnectionString("DB");
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Brand>(entity =>
