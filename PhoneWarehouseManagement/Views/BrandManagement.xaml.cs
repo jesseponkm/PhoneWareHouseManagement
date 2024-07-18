38eb24b09c33ae4db925b96d034dfc2d7d939716
jesseponkm <thuatdthe173070@fpt.edu.vn>
2024-07-18 16:04:05 +0700

    thuat

---
+++ PhoneWarehouseManagement/Views/BrandManagement.xaml.cs
@@ -0,0 +1,35 @@
+﻿using BusinessObjects.Models;
+using Services;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace PhoneWarehouseManagement.Views
+{
+    /// <summary>
+    /// Interaction logic for BrandManagement.xaml
+    /// </summary>
+    public partial class BrandManagement : Window
+    {
+        private readonly IBrandService brandService;
+        public BrandManagement()
+        {
+            InitializeComponent();
+            brandService = new BrandService();
+            loadBrand();
+
+        }
+        public void loadBrand()
+        {
+            grdBrand.ItemsSource = brandService.GetBrands();
+        }
+
+        private void grdBrand_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (grdBrand.SelectedItem is Brand selected)
+            {
+                txtBrandId.Text = selected.BrandId.ToString();
+                txtBrandName.Text = selected.BrandName.ToString();
+            }
+        }
+    }
+}
